e8ad19b14d15a38391371eed72c286c5e84abc34
Nicolas Gonzalez <nicogonzalez2000@gmail.com>
2021-11-13 22:59:08 -0300

    Actualizacion del proyecto

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
+//La idea es hacer un Login, con una pequeña tienda. Todo ambientado en un Hospital con diferentes planes médicos.
 
-function login(){
-
-    let user = prompt("Ingrese el usuario: (Pista: pepe)");
-
-    while (user != "pepe"){
-
-        user = prompt("Ingrese el usuario: (Pista: pepe)");
-
-    }
-
-    let pass = prompt("Ingrese la contraseña: (Pista: 123)");
-
-    while (pass != "123"){
-
-        pass = prompt("Ingrese la contraseña: (Pista: 123)");
-        
-    }
-
-    alert(user+" has ingresado con la contraseña "+pass)
-
+function usuario(user, contrasena) {
+    this.user = user;
+    this.contrasena = contrasena;
 }
 
-login();
+const usuario1 = new usuario ("nico","1234");
+const usuario2 = new usuario ("pablo","4321");
+const usuario3 = new usuario ("juan","1122");
 
-//La idea es hacer un Login, con una pequeña tienda. Todo ambientado en un Hospital con diferentes planes médicos.
+function login(){  
+
+    var user = document.getElementById("exampleInputEmail1").value;
+    var contrasena = document.getElementById("exampleInputPassword1").value;
+
+
+    if (user==usuario1.user && contrasena==usuario1.contrasena) { 
+        window.location="pages/principal.html"; 
+    } 
+    if (user==usuario2.user && contrasena==usuario2.contrasena) { 
+        window.location="pages/principal.html"; 
+    } 
+    if (user==usuario3.user && contrasena==usuario3.contrasena) { 
+        window.location="pages/principal.html";
+    } 
+    if (user=="" && contrasena=="") { 
+        alert("Ingrese los datos!");
+    } 
+    }
